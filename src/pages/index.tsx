634a826092defa4464a286200f55d4b479981e5c
Mark Ryan <markryandelarmente104@gmail.com>
2023-03-08 17:56:35 +0800

    not allowed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -163,7 +163,7 @@ const Home: NextPage = () => {
           <button
             disabled={text ? false : true}
             onClick={handleSubmit}
-            className={`group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-lg font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-300 ${
+            className={`group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-lg font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-not-allowed disabled:bg-gray-300 ${
               flag ? "mr-9" : ""
             }`}
           >
